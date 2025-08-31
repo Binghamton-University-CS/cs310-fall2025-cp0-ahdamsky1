090e02ccfab17a6e96f2102e6c6f35bcfc0e3a49
adamsky1 <adamsky1@cs.binghamton.edu>
2025-08-31 15:39:52 -0400

    Commit CP0

--- zoo.cpp
+++ zoo.cpp
@@ -1,34 +1,22 @@
 #include <iostream>
 #include <stdlib.h>
-using namespace std;
-
-class Animal {
-   public:
-      Animal(string speciesName, unsigned int discoveryYear) {
-         species = speciesName;
-         year_discovered = discoveryYear;
-      }
-
-      Animal() : species(""), year_discovered(0) {};
-
-      void display() {
-         cout << species << " [" << year_discovered << "]" << endl;
-      }
-
-   private:
-      string species = "";
-      unsigned int year_discovered = 0;
-};
+#include "AnimalsInZoo.h"
 
 int main() {
-   Animal *animal1 = new Animal("African Elephant", 1758);
-   Animal animal2("Giant Panda", 1869);
+   	Animal *animal1 = new Animal("African Elephant", 1758);
+   	Animal animal2("Giant Panda", 1869);
+   	Animal animal3("African Lion", 1758);
 
-   delete animal1;
-   animal1 = new Animal("Snow Leopard", 1777);
+   	delete animal1;
+   	animal1 = new Animal("Snow Leopard", 1777);
 
-   animal2.display();
-   animal1->display();
+   	animal2.display();
+   	animal1->display();
+   	animal3.display();
 
-   delete animal1;
+   	delete animal1;
+
+
+	AnimalsInZoo animalsinzoo(animal2);
+	animalsinzoo.display();
 }
